fcdd7131344233d4fbf9fdf3682615f3acaad1a2
Djordje Mitrovic <52535273+djole21@users.noreply.github.com>
2023-08-14 21:55:06 +0200

    Init app (#1)
    Init app

---
+++ src/test/java/dev/djmitrovic/spotifyrecommender/SpotifyRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.djmitrovic.spotifyrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
